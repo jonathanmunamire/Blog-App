99d5d308980995268e452ea15311e6aa66065475
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-02 01:45:37 +0200

    Fix Linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    after_create :update_comments_counter
+  after_create :update_comments_counter
 
-    private
+  private
 
-    def update_comments_counter
-        post.increment!(:comments_counter)
-    end
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,12 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    after_create :update_likes_counter
+  after_create :update_likes_counter
 
-    private
+  private
 
-    def update_likes_counter
-        post.increment!(:likes_counter)
-    end
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
