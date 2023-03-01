e86b146730ff9178f82bfddad523bbba8c0ba455
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-02 01:44:41 +0200

    Process Data

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+
+    after_create :update_posts_counter
+
+    def five_most_recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
+
+    private
+
+    def update_posts_counter
+        author.increment!(:posts_counter)
+    end
+end
