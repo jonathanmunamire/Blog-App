8d8af5184414e624b2ea0ff57eac3a8e35ac731d
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-03 22:17:21 +0200

    Add Validation and Tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
 
   after_create :update_posts_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_inter: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_inter: true, greater_than_or_equal_to: 0 }
+
   def five_most_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
