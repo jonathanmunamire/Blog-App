9af1fa2daba88d7937b3f28dcf0c24e6223df5c1
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-09 16:21:38 +0200

    Add Tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get('/users/1/posts') }
+    it 'should return success status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'it renders index template' do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get('/users/1/posts/1') }
+    it 'should return success status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'it renders index template' do
+      expect(response).to render_template(:show)
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get users_path }
+    it 'should return success status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'it renders index template' do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get('/users/1') }
+    it 'should return success status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'it renders index template' do
+      expect(response).to render_template(:show)
+    end
   end
 end
