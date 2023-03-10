452e7cf1b087eafc346787277583f9d7c6e232e7
jonathanmunamire <jonathanmunamire@gmail.com>
2023-03-10 16:14:05 +0200

    Implement Views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.where(author_id: params[:user_id])
+    @user = User.find(params[:user_id])
   end
 
   def show
-    @post = Post.find_by(id: params[:id])
+    @post = Post.find_by(author_id: params[:user_id], id: params[:id])
+    @user = User.find(params[:user_id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  layout 'standard'
   def index
     @users = User.all
   end
